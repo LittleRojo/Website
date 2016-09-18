97ece3aa8df8698bb8fe6243bad49927850162b5
Administrator <administrator@littlerojo.com>
2016-09-18 01:18:42 -0500

    picture

--- me/tojo10.js
+++ me/tojo10.js
@@ -23,50 +23,67 @@ tojo10.prototype.SetupScene = function() {
 		canvas.height = img.height;
 		ctx.drawImage(img,0,0);
 		
+		var layer = new THREE.Geometry();				
+		//layer.addAttribute("color", new THREE.BufferAttribute(pixels.color, 4));
+		//layer.addAttribute("position", new THREE.BufferAttribute(positions, 3));
+		//layer.addAttribute("size", new THREE.BufferAttribute(pixels.size, 1));
+		layer.verticesNeedUpdate = true;
+		layer.normalsNeedUpdate = true;
+		layer.colorsNeedUpdate = true;
+		layer.uvsNeedUpdate = true;
+		
+		App.tojo.layers.push(layer);
+
 		var counter = 0;
-		var pixels = img.width * img.height;
-		var positions = new Float32Array( pixels * 3 );
-		var colors = new Float32Array( pixels * 4 );
-		var sizes = new Float32Array( pixels );
+		var pixels = new Pixel({
+			position: new Float32Array( img.width * img.height * 3 ),
+			color: [],
+			size: new Float32Array( img.width * img.height )
+		});
 		for(var x = 0; x < img.width; x++){
 			for(var y = 0; y < img.height; y++) {
 				var pixel = ctx.getImageData(x, y, 1, 1);
 
-				colors[ 4 * counter ] = pixel.data[0];
-				colors[ 4 * counter + 1 ] = pixel.data[1];
-				colors[ 4 * counter + 2 ] = pixel.data[2];
-				colors[ 4 * counter + 3 ] = pixel.data[3];
+				pixels.color[counter] = new THREE.Color("rgb(" + pixel.data[0] + "," + pixel.data[1] + "," + pixel.data[2] + ")");
+				//pixels.color[counter].setHSL( Math.random(), 1.0, 0.5 );
+		
+				/*pixels.color[ 4 * counter ] = pixel.data[0];
+				pixels.color[ 4 * counter + 1 ] = pixel.data[1];
+				pixels.color[ 4 * counter + 2 ] = pixel.data[2];
+				pixels.color[ 4 * counter + 3 ] = pixel.data[3];*/
 
-				positions[ 3 * counter ] = x - (img.width / 2);
-				positions[ 3 * counter + 1 ] = y - (img.height / 2);
-				positions[ 3 * counter+ 2 ] = 0;//( Math.random() * 2 - 1 ) * this.radius;
+				pixels.position[ 3 * counter ] = x - (img.width / 2);
+				pixels.position[ 3 * counter + 1 ] = y - (img.height / 2);
+				pixels.position[ 3 * counter+ 2 ] = 0;//( Math.random() * 2 - 1 ) * this.radius;
 
-				sizes[counter] = 1;
+				pixels.size[counter] = 1;
+
+				layer.vertices.push(new THREE.Vector3(pixels.position[ 3 * counter ], pixels.position[ 3 * counter + 1 ], pixels.position[ 3 * counter + 2 ]))
 				counter++;
 			}
 		}
 
-		var layer = new THREE.BufferGeometry();				
-		layer.addAttribute("color", new THREE.BufferAttribute(colors, 4));
-		layer.addAttribute("position", new THREE.BufferAttribute(positions, 3));
-		layer.addAttribute("size", new THREE.BufferAttribute(sizes, 1));
-		App.tojo.layers.push(layer);
-
-		var material = new THREE.ShaderMaterial( 
+		layer.colors = pixels.color;
+		var material = new THREE.PointsMaterial( 
 		{
-			uniforms: {
-				time: { value: 1.0 }
-			},
-			vertexShader:   document.getElementById( 'vertexshader' ).textContent,
-			fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
-
-			//blending:       THREE.AdditiveBlending,
-			//depthTest:      false,
-			transparent:    true
+			color: 0xffffff,
+			size: 2.7478,
+			blending: THREE.AdditiveBlending,
+			vertexColors: THREE.VertexColors,
+			//map: THREE.ImageUtils.loadTexture('spark1.png'),
+			opacity: 1,
+			transparent: true
 		});
-		material.needsUpdate = true;
+		//material.needsUpdate = true;
 
 		var particleSystem = new THREE.Points(layer, material);
+		//layer.attributes.color.needsUpdate = true;
+		//layer.attributes.size.needsUpdate = true;
+		//layer.attributes.position.needsUpdate = true;
+		//material.needsUpdate = true;
+		
+		//var particleSystem = new THREE.Mesh(layer, material);
+		//particleSystem.geometry.attributes.color.needsUpdate = true;
 		App.tojo.particleSystems.push(particleSystem);
 		App.tojo.scene.add(particleSystem);		
 
@@ -74,7 +91,7 @@ tojo10.prototype.SetupScene = function() {
 		App.tojo.AnimateScene();
 	};
 	img.style.display = "none";
-	img.src = 'logo.png';			
+	img.src = 'Daddy.png';			
 }	
 
 tojo10.prototype.RedrawScene = function() {
@@ -88,13 +105,13 @@ tojo10.prototype.RedrawScene = function() {
 
 tojo10.prototype.RedrawSceneFrame = function() {	
 	for(var a = 0; a < this.layers.length; a++) {
-		var position = this.particleSystems[a].geometry.attributes.position.array;
-		for ( var i = 0; i < position.length; i++ ) {
-			position[ 3 * i + 0 ] += i * .00001;
+		//var position = this.particleSystems[a].geometry.attributes.position.array;
+		//for ( var i = 0; i < position.length; i++ ) {
+			//position[ 3 * i + 0 ] += i * .00001;
 			//position[ 3 * i + 1 ] += i * .00001;//( Math.random() * 8 - 1 ) * this.radius;
 			//position[ 3 * i + 2 ] += 0;//( Math.random() * 2 - 1 ) * this.radius;
-		}		
-		this.particleSystems[a].geometry.attributes.position.needsUpdate = true;
+		//}		
+		this.particleSystems[a].geometry.verticesNeedUpdate = true;
 	}
 }
   
