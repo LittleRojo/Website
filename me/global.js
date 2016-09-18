7885433451bbd3fb9a62564d13dde66fc0b3e9d8
Administrator <administrator@littlerojo.com>
2016-09-17 22:54:32 -0500

    shader

--- me/global.js
+++ me/global.js
@@ -2,9 +2,8 @@
 function onload(){
 	var main = document.getElementById( 'mainCanvas' );
 	App = new Set();
-	App.Stage(main);
 	var tojo = new tojo10();
-	App.AddScene(tojo);	
+	App.Stage(main, tojo);
 }
 
 function play(){
