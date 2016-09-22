c24f15a73326a5e0c568902dd3c999579b633c3f
Administrator <administrator@littlerojo.com>
2016-09-22 12:53:08 -0500

    Commit19

--- me/tojo10.js
+++ me/tojo10.js
@@ -276,10 +276,10 @@ tojo10.prototype.UpdateMusic = function() {
 	var delta = clock.getDelta() * 1000;
 	clockTime += delta;
 	if(clockTime > 1000) {
-		if(!App.Sound.on) {
+		/*if(!App.Sound.on) {
 			App.Sound.Load();
 			App.Sound.Play();
-		}			
+		}*/			
 		clockTime = 0;
 	}
 }
