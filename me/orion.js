b62e6411247e8886f78ff741e7b1bd193675938f
Administrator <administrator@littlerojo.com>
2016-09-22 03:01:14 -0500

    Commit15

--- me/orion.js
+++ me/orion.js
@@ -1,53 +1,98 @@
 function Sound(bpm) {
-    this.bpm = bpm;
-    this.song = [];   
+    this.conductor = new BandJS();
+    this.conductor.setTickerCallback(function(seconds) {
+        
+    });
+        this.conductor.setOnFinishedCallback(function() {
+    
+    });
+
+    this.conductor.setOnDurationChangeCallback(function() {
+        var totalSeconds = this.conductor.getTotalSeconds();
+    });
+    this.rightHand = this.conductor.createInstrument('sine', 'oscillators');
+    this.leftHand = this.conductor.createInstrument('sine', 'oscillators');       
 }
 
 Sound.prototype.Load = function() {
-    /*this.song.push(new Note("D3", 8));
-    this.song.push(new Note("F5", 12));
-    this.song.push(new Note("E5", 3));
-    this.song.push(new Note("REST"));
+    this.conductor.setMasterVolume(50);
+    this.conductor.setTimeSignature(4, 4);
+    this.conductor.setTempo(90);  
 
-    this.song.push(new Note("D3", 8));
-    this.song.push(new Note("F5", 12));
-    this.song.push(new Note("E5", 3));
-    this.song.push(new Note("REST"));*/
+    var octave = 4;
+    this.rightHand.setVolume(25);
+    this.rightHand.note('half', 'C' + octave)
+        .note('half', 'C' + octave)
+        .note('half', 'G' + octave)
+        .note('half', 'G' + octave)
+        .note('half', 'A' + octave)
+        .note('half', 'A' + octave)
+        .note('whole', 'G' + octave)
+        
+        .rest('eighth')
+        .note('half', 'F' + octave)
+        .note('half', 'F' + octave)
+        .note('half', 'E' + octave)
+        .note('half', 'E' + octave)
+        .note('half', 'D' + octave)
+        .note('half', 'D' + octave)
+        .note('whole', 'C' + octave)
+        
+        .rest('eighth')
+        .note('half', 'G' + octave)
+        .note('half', 'G' + octave)
+        .note('half', 'F' + octave)
+        .note('half', 'F' + octave)
+        .note('half', 'E' + octave)
+        .note('half', 'E' + octave)
+        .note('whole', 'D' + octave)
+        
+        .rest('eighth')
+        .note('half', 'G' + octave)
+        .note('half', 'G' + octave)
+        .note('half', 'F' + octave)
+        .note('half', 'F' + octave)
+        .note('half', 'E' + octave)
+        .note('half', 'E' + octave)
+        .note('whole', 'D' + octave)
+        
+        .rest('eighth')
+        .note('half', 'C' + octave)
+        .note('half', 'C' + octave)
+        .note('half', 'G' + octave)
+        .note('half', 'G' + octave)
+        .note('half', 'A' + octave)
+        .note('half', 'A' + octave)
+        .note('whole', 'G' + octave)
+        
+        .rest('eighth')
+        .note('half', 'F' + octave)
+        .note('half', 'F' + octave)
+        .note('half', 'E' + octave)
+        .note('half', 'E' + octave)
+        .note('half', 'D' + octave)
+        .note('half', 'D' + octave)
+        .note('whole', 'C' + octave)
+        .rest('whole');
 
-    this.song.push(new Note("D3", 8));
-    this.song.push(new Note("F5", 12));
-    this.song.push(new Note("E5", 3));
-    this.song.push(new Note("D5", 3));
-    this.song.push(new Note("B5", 3));
-    this.song.push(new Note("E5", 3));
-    this.song.push(new Note("REST"));
-    
+    this.rightHand.repeatFromBeginning(1000);
+
+    /*octave = 4;
+    this.leftHand.setVolume(50);
+    this.leftHand.note('eighth', 'C' + octave)
+        .note('eighth', 'C' + octave)
+        .note('eighth', 'C' + octave)
+        .note('eighth', 'C' + octave)
+        .note('eighth', 'C' + octave)
+        .note('eighth', 'C' + octave)
+        .note('eighth', 'C' + octave)
+        .note('eighth', 'C' + octave);*/
+
+     
+
+    this.player = this.conductor.finish();  
 }
 
-var elapsedTime = 0;
-var clock = new THREE.Clock();
-Sound.prototype.Play = function() {
-    var context = new (window.AudioContext || window.webkitAudioContext)();
-
-    this.vol = context.createGain();
-    this.vol.gain.value = 1; // from 0 to 1, 1 full volume, 0 is muted    
-    this.vol.connect(context.destination); // connect vol to context destination
-
-    this.osc = context.createOscillator(); // instantiate an oscillator
-    this.osc.connect(this.vol); // connect osc to vol    
-    this.osc.frequency.value = 7.83;    
-    this.osc.start(context.currentTime); // start it three seconds from now
-
-    /*setInterval(function() {
-        App.Sound.osc.frequency.value = Math.random() * 500;
-        setTimeout(function(){
-            if(App.Sound.vol.gain.value == 0) {
-                App.Sound.vol.gain.value = .04;
-            }
-            else {
-                App.Sound.vol.gain.value = 0;
-            }         
-            App.Sound.osc.frequency.value = Math.random() * 500;          
-        }, Math.random() * 1000);
-    }, 1000); 	*/
+Sound.prototype.Play = function() {     
+    this.player.play();
 }
