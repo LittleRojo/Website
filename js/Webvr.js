b69863bfec555f71e5815ecd112ca910e91d83e5
Administrator <administrator@littlerojo.com>
2016-10-17 03:37:26 -0500

    3

--- js/Webvr.js
+++ js/Webvr.js
@@ -87,7 +87,7 @@ var WEBVR = {
 		button.style.fontStyle = 'normal';
 		button.style.textAlign = 'center';
 		button.style.zIndex = '999';
-		button.style.backgroundImage = 'url(\"img/vrLogoIcon.png\")';
+		button.style.backgroundImage = "url('img/vrLogoIcon.png')";
 		button.onclick = function() {
 
 			effect.isPresenting ? effect.exitPresent() : effect.requestPresent();
