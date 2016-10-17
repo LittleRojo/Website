9541662e6d21005e5e2f26cf8ecd7bdf05ce1676
Administrator <administrator@littlerojo.com>
2016-10-17 18:29:29 -0500

    9

--- V2/me/App.js
+++ V2/me/App.js
@@ -111,7 +111,7 @@ App.prototype.createApp = function(fileName) {
 		}
 		else {	
 		}
-		App.renderer.render( App.scene, App.camera );
+		App.effect.render( App.scene, App.camera );
 	}, false);
 }
 
