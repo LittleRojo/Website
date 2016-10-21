1365cfffa92dda950ce78d3d94859ca9832dbd0e
Administrator <administrator@littlerojo.com>
2016-10-21 03:20:57 -0500

    17

--- Please/me/global.js
+++ Please/me/global.js
@@ -49,8 +49,8 @@ function onload(pageNumber){
     hemiLight();
     directionalLight();
     spotLight();
-    /*carpet();
-    desk();*/
+    carpet();
+    desk();
 
 	/*this.getPlane( {
         x: 0,
