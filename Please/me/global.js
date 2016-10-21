a0d38d5bc63203f571b6b06614acc1b4fa5c4eff
Administrator <administrator@littlerojo.com>
2016-10-21 02:05:08 -0500

    2

--- Please/me/global.js
+++ Please/me/global.js
@@ -1,109 +1,22 @@
 window.addEventListener( 'resize', onWindowResize, false );
 
-var page;
 function onload(pageNumber){
-
 	var mainCanvas = document.getElementById( 'mainCanvas' );
 	mainCanvas.style.visibility = 'visible';
 
 	App = new Set();
 	var tojo = new tojo12();	
-	App.Stage(mainCanvas, tojo);
-
-	
-}
-
-var xRoot = .00386699;
-var yRoot = .00386699;
-var zRoot = .00386699; 
-var animationRoot = .00386699;
-function updateCamera(button) {
-	App.tojo.animationSpeed = animationRoot * button.value * 440;
-
-	//var speed = document.getElementById( 'speed' );
-	//speed.style.opacity =  (speed.value / 2) * .7 + .3;
-}
-
-function updateSpeed(button) {
-	xStepFactor = xRoot * button.value
-	yStepFactor = yRoot * button.value
-	zStepFactor = zRoot * button.value;
-
-	//var speed = document.getElementById( 'speed' );
-	//camera.style.opacity =  (speed.value / 2) * .7 + .3;
-}
-
-function updateOddball(button) {
+	App.Stage(mainCanvas, tojo);	
 }
 
 function onWindowResize(){
     App.camera.aspect = window.innerWidth / window.innerHeight;
     App.camera.updateProjectionMatrix();
 	App.renderer.setSize( window.innerWidth, window.innerHeight );
-
-	if(("standalone" in window.navigator) && window.navigator.standalone) {
-		//IPHONE ORIENTATION - PROFILE
-		if(window.orientation === 90 || window.orientation === -90) {			
-			var main = document.getElementById( 'mainCanvas' );
-			var title = document.getElementById( 'navigation' );
-		}
-
-		//IPHONE ORIANTATION - LANDSCAPE
-		else {
-			var main = document.getElementById( 'mainCanvas' );
-			var title = document.getElementById( 'navigation' );
-		} 		
-	}
-	else {		
-		var main = document.getElementById( 'mainCanvas' );
-		var title = document.getElementById( 'navigation' );
-	}
-
-	if(page == "10") {
-		var speed = document.getElementById( 'speed' );
-		speed.style.width = ((window.innerWidth - 0) / 3);
-		speed.style.left = 0;
-		speed.style.top = window.innerHeight - 25;
-
-		var camera = document.getElementById( 'camera' );
-		camera.style.width = ((window.innerWidth - 0) / 3);
-		camera.style.left = ((window.innerWidth - 0) / 3);
-		camera.style.top = window.innerHeight - 25;
-		camera.style.visibility = "visible";
-
-		var oddball = document.getElementById( 'oddball' );
-		oddball.style.width = ((window.innerWidth - 0) / 3);
-		oddball.style.left = ((window.innerWidth - 0) / 3) * 2;
-		oddball.style.top = window.innerHeight - 25;
-		oddball.style.visibility = "visible";
-
-		//var sound = document.getElementById( 'soundButton' );
-		//sound.style.left = (((window.innerWidth - ((((window.innerWidth - 100) / 3) * 3))) / 2) - 19) + ((window.innerWidth - 100) / 3) * 3;
-		//sound.style.top = window.innerHeight - 42;
-	}
-	else if(page == "11") {
-		
-	}
-	else if(page == "12") {
-		App.camera.aspect = window.innerWidth / window.innerHeight;
-		App.camera.updateProjectionMatrix();
-		App.effect.setSize( window.innerWidth, window.innerHeight );
-	}
+	
+	App.camera.aspect = window.innerWidth / window.innerHeight;
+	App.camera.updateProjectionMatrix();
+	App.effect.setSize( window.innerWidth, window.innerHeight );
 
 	App.UpdateScene();
-}
-
-function play() {
-	if(App.audiostate == 0){     
-		App.audio.play();
-		App.audiostate = 1;
-		var img = document.getElementById('soundButton')
-		img.src = "img/play.png";
-	}
-	else {
-		App.audio.pause();
-		App.audiostate = 0;
-		var img = document.getElementById('soundButton')
-		img.src = "img/off.png";
-	}
 }
