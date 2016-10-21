85b171edaa8eab0c104558c2e3d237ac780440b6
Administrator <administrator@littlerojo.com>
2016-10-21 02:33:01 -0500

    5

--- Please/me/global.js
+++ Please/me/global.js
@@ -1,5 +1,17 @@
 window.addEventListener( 'resize', onWindowResize, false );
 
+window.addEventListener( 'mousedown', onMouseDown, false );
+window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener( 'mouseup', onMouseUp, false );
+
+window.addEventListener( 'DOMMouseScroll', onMouseWheel, false ); 
+
+window.addEventListener( 'touchstart', onTouchStart, false );
+window.addEventListener( 'touchmove', onTouchMove, false );
+window.addEventListener( 'touchend', onTouchEnd, false );
+
+window.addEventListener( 'keydown', onKeyDown, false );
+
 function onload(pageNumber){
 	var mainCanvas = document.getElementById( 'mainCanvas' );
 	mainCanvas.style.visibility = 'visible';
@@ -19,4 +31,36 @@ function onWindowResize(){
 	App.effect.setSize( window.innerWidth, window.innerHeight );
 
 	App.UpdateScene();
+}
+
+function onMouseDown() {
+
+}
+
+function onMouseMove() {
+
+}
+
+function onMouseUp() {
+	
+}
+
+function onMouseWheel() {
+
+}
+
+function onTouchStart() {
+
+}
+
+function onTouchMove() {
+	
+}
+
+function onTouchEnd() {
+	
+}
+
+function onKeyDown() {
+	
 }
