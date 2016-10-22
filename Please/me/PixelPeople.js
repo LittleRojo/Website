def8e46480337f05cb3b75a1d1837855ad1e5211
Administrator <administrator@littlerojo.com>
2016-10-21 21:59:57 -0500

    61

--- Please/me/PixelPeople.js
+++ Please/me/PixelPeople.js
@@ -77,7 +77,7 @@ function AnimateScene(delta) {
 function onWindowResize(){
 	if(vrButton !== undefined) {
 		vrButton.style.left = window.innerWidth / 2 - 32 + 'px';
-		vrButton.style.top = window.innerHeight - 44 + 'px';
+		vrButton.style.top = window.innerHeight - 58 + 'px';
 	}
 
     camera.aspect = window.innerWidth / window.innerHeight;
