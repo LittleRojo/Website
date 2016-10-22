aec663a39676284f94e17543f8b62876b15003bd
Administrator <administrator@littlerojo.com>
2016-10-21 23:13:44 -0500

    60

--- Please/me/PixelPeople.js
+++ Please/me/PixelPeople.js
@@ -83,7 +83,7 @@ function onWindowResize(){
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	effect.setSize( window.innerWidth, window.innerHeight );
+	//effect.setSize( window.innerWidth, window.innerHeight );
 
 	effect.render( scene, camera )
 }
