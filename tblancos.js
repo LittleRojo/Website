8727d250b90d629eb773f1bb657cbf05c71a8862
Ryan Matthew <administrator@littlerojo.com>
2017-03-12 02:45:18 -0500

    maps test

--- tblancos.js
+++ tblancos.js
@@ -2,3 +2,12 @@ function start() {
 	//var element = document.getElementById("buffer");
 	//element.style.height = window.innerHeight + "px";
 }
+
+function initMap() {
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 32.956175, lng: -96.830576},
+	  scrollwheel: true,
+	  zoom: 8
+	});
+}
