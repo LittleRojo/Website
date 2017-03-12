39cd8da0736da9a0a900315668e6097d790ba40c
Ryan Matthew <administrator@littlerojo.com>
2017-03-12 03:31:45 -0500

    Google Maps

--- tblancos.js
+++ tblancos.js
@@ -5,9 +5,29 @@ function start() {
 
 function initMap() {
 	// Create a map object and specify the DOM element for display.
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: 32.956175, lng: -96.830576},
+	var cafeMap = new google.maps.Map(document.getElementById('cafeMap'), {	  
+	  center: { lat: 32.407716, lng: -94.716789 },
 	  scrollwheel: true,
-	  zoom: 8
+	  zoom: 10
+	});	
+	var cafeMapTraffic = new google.maps.TrafficLayer();
+        cafeMapTraffic.setMap(cafeMap);		
+	var cafeMarker = new google.maps.Marker({
+	  position: { lat: 32.407716, lng: -94.716789 },
+	  map: cafeMap,
+	  title: 'T.Blanco\'s Cafe'
+	});
+		
+	var cantinaMap = new google.maps.Map(document.getElementById('cantinaMap'), {
+		center: { lat: 32.956175, lng: -96.830576 },
+		scrollwheel: true,
+		zoom: 10
+	});
+	var cantinaMapTraffic = new google.maps.TrafficLayer();
+        cantinaMapTraffic.setMap(cantinaMap);	
+	var cantinaMarker = new google.maps.Marker({
+	  position: { lat: 32.956175, lng: -96.830576 },
+	  map: cantinaMap,
+	  title: 'T.Blanco\'s Cantina'
 	});
 }
