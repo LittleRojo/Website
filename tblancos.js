f87978d213427445810a178b824bd3454ddf0a55
Ryan Matthew <administrator@littlerojo.com>
2017-03-14 11:45:32 -0500

    Live Google Map Directions

--- tblancos.js
+++ tblancos.js
@@ -1,4 +1,4 @@
-window.onresize = function(event) {
+window.onresize = function(event) {	
 	cafeMapElement = document.getElementById('cafeMap');
 	cafeMapElement.style.height = ((window.innerHeight / 2) * .75) + "px";
 
@@ -26,7 +26,7 @@ function onLoad() {
 function initMap() {
 	window.onresize();
 	
-	var cafeMap = new google.maps.Map(cafeMapElement, {	  
+	/*var cafeMap = new google.maps.Map(cafeMapElement, {	  
 	  center: { lat: 32.407716, lng: -94.716789 },
 	  scrollwheel: true,
 	  zoom: 10
@@ -50,24 +50,27 @@ function initMap() {
 	  position: { lat: 32.956175, lng: -96.830576 },
 	  map: cantinaMap,
 	  title: 'T.Blanco\'s Cantina'
-	});
+	});*/
 	
 	var infoWindow = new google.maps.InfoWindow({map: cafeMap});
 
 	// Try HTML5 geolocation.
 	if (navigator.geolocation) {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-		var pos = {
-		  lat: position.coords.latitude,
-		  lng: position.coords.longitude
-		};
+		//var location_timeout = setTimeout("geolocFail()", 10000);
+				
+		navigator.geolocation.getCurrentPosition(function(position) {
+			//clearTimeout(location_timeout);
+			var pos = {
+			  lat: position.coords.latitude,
+			  lng: position.coords.longitude
+			};
 
-		infoWindow.setPosition(pos);
-		infoWindow.setContent('Location found.');
-		cafeMap.setCenter(pos);
-	  }, function() {
-		//handleLocationError(true, infoWindow, cafeMap.getCenter());
-	  });
+			infoWindow.setPosition(pos);
+			infoWindow.setContent('Location found.');
+			cafeMap.setCenter(pos);
+		}, function() {
+			//handleLocationError(true, infoWindow, cafeMap.getCenter());
+		});
 	} else {
 	  // Browser doesn't support Geolocation
 	  //handleLocationError(false, infoWindow, cafeMap.getCenter());
