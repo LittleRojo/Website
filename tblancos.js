3661be9af4d4e1225021d6231868d0d0104022ec
Ryan Matthew <administrator@littlerojo.com>
2017-03-14 11:01:28 -0500

    current location test

--- tblancos.js
+++ tblancos.js
@@ -18,11 +18,6 @@ window.onresize = function(event) {
 	
 	var menuTableElement = document.getElementById('menuTable');
 	menuTableElement.style.height = (window.innerWidth / 25) + "px"
-	
-	var menuItems = document.getElementsByClassName('menuItem');
-	for (var a = 0; a < menuItems.length; a++) {
-		menuItems[a].style.fontSize = (window.innerWidth / 500) + "em";
-	}
 }
 
 function onLoad() {
@@ -56,4 +51,32 @@ function initMap() {
 	  map: cantinaMap,
 	  title: 'T.Blanco\'s Cantina'
 	});
+	
+	var infoWindow = new google.maps.InfoWindow({map: cafeMap});
+
+	// Try HTML5 geolocation.
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+		var pos = {
+		  lat: position.coords.latitude,
+		  lng: position.coords.longitude
+		};
+
+		infoWindow.setPosition(pos);
+		infoWindow.setContent('Location found.');
+		cafeMap.setCenter(pos);
+	  }, function() {
+		//handleLocationError(true, infoWindow, cafeMap.getCenter());
+	  });
+	} else {
+	  // Browser doesn't support Geolocation
+	  //handleLocationError(false, infoWindow, cafeMap.getCenter());
+	}
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+						  'Error: The Geolocation service failed.' :
+						  'Error: Your browser doesn\'t support geolocation.');
+  }
