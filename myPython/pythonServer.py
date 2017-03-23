54d2ab32de7130ccb2d39fcbc4939eb587327775
Ryan Matthew <administrator@littlerojo.com>
2017-03-23 01:15:15 -0500

    socket working locally

--- myPython/pythonServer.py
+++ myPython/pythonServer.py
@@ -1,42 +1,117 @@
-from gevent import monkey
-monkey.patch_all()
+# set async_mode to 'threading', 'eventlet', 'gevent' or 'gevent_uwsgi' to
+# force a mode else, the best mode is selected automatically from what's
+# installed
+async_mode = None
 
-import cgi
-import redis
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+import time
+from flask import Flask, render_template
+import socketio
 
+sio = socketio.Server(logger=True, async_mode=async_mode)
 application = Flask(__name__)
-db = redis.StrictRedis('localhost', 6379, 0)
-socketio = SocketIO(application)
+application.wsgi_app = socketio.Middleware(sio, application.wsgi_app)
+application.config['SECRET_KEY'] = 'secret!'
+thread = None
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        sio.sleep(10)
+        count += 1
+        sio.emit('my response', {'data': 'Server generated event'},
+                 namespace='/test')
 
 
 @application.route('/')
 def main():
+    global thread
+    if thread is None:
+        thread = sio.start_background_task(background_thread)
     return render_template('main.html')
 
 
-@application.route('/pymeetups/')
-def pymeetups():
-    return render_template('pymeetups.html')
+@sio.on('my event', namespace='/test')
+def test_message(sid, message):
+    sio.emit('my response', {'data': message['data']}, room=sid,
+             namespace='/test')
 
 
-@socketio.on('connect', namespace='/dd')
-def ws_conn():
-    c = db.incr('connected')
-    socketio.emit('msg', {'count': c}, namespace='/dd')
+@sio.on('my broadcast event', namespace='/test')
+def test_broadcast_message(sid, message):
+    sio.emit('my response', {'data': message['data']}, namespace='/test')
 
 
-@socketio.on('disconnect', namespace='/dd')
-def ws_disconn():
-    c = db.decr('connected')
-    socketio.emit('msg', {'count': c}, namespace='/dd')
+@sio.on('join', namespace='/test')
+def join(sid, message):
+    sio.enter_room(sid, message['room'], namespace='/test')
+    sio.emit('my response', {'data': 'Entered room: ' + message['room']},
+             room=sid, namespace='/test')
 
-@socketio.on('city',  namespace='/dd')
-def ws_city(message):
-    print(message['city'])
-    socketio.emit('city', {'city': cgi.escape(message['city'])},
-                  namespace="/dd")
+
+@sio.on('leave', namespace='/test')
+def leave(sid, message):
+    sio.leave_room(sid, message['room'], namespace='/test')
+    sio.emit('my response', {'data': 'Left room: ' + message['room']},
+             room=sid, namespace='/test')
+
+
+@sio.on('close room', namespace='/test')
+def close(sid, message):
+    sio.emit('my response',
+             {'data': 'Room ' + message['room'] + ' is closing.'},
+             room=message['room'], namespace='/test')
+    sio.close_room(message['room'], namespace='/test')
+
+
+@sio.on('my room event', namespace='/test')
+def send_room_message(sid, message):
+    sio.emit('my response', {'data': message['data']}, room=message['room'],
+             namespace='/test')
+
+
+@sio.on('disconnect request', namespace='/test')
+def disconnect_request(sid):
+    sio.disconnect(sid, namespace='/test')
+
+
+@sio.on('connect', namespace='/test')
+def test_connect(sid, environ):
+    sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid,
+             namespace='/test')
+
+
+@sio.on('disconnect', namespace='/test')
+def test_disconnect(sid):
+    print('Client disconnected')
+
 
 if __name__ == '__main__':
-    socketio.run(application, "0.0.0.0", port=9902)
+    if sio.async_mode == 'threading':
+        # deploy with Werkzeug
+        application.run(threaded=True)
+    elif sio.async_mode == 'eventlet':
+        # deploy with eventlet
+        import eventlet
+        import eventlet.wsgi
+        eventlet.wsgi.server(eventlet.listen(('', 9902)), application)
+    elif sio.async_mode == 'gevent':
+        # deploy with gevent
+        from gevent import pywsgi
+        try:
+            from geventwebsocket.handler import WebSocketHandler
+            websocket = True
+        except ImportError:
+            websocket = False
+        if websocket:
+            pywsgi.WSGIServer(('', 9902), application,
+                              handler_class=WebSocketHandler).serve_forever()
+        else:
+            pywsgi.WSGIServer(('', 9902), application).serve_forever()
+    elif sio.async_mode == 'gevent_uwsgi':
+        print('Start the application through the uwsgi server. Example:')
+        print('uwsgi --http :9902 --gevent 1000 --http-websockets --master '
+              '--wsgi-file app.py --callable app')
+    else:
+        print('Unknown async_mode: ' + sio.async_mode)
